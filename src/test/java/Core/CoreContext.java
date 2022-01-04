e4647b542d3ecd3246c0f8ece1864e7cb28b7354
Riza P Nansuri <riza.nansuri@dana.id>
2022-01-04 10:47:27 +0700

    Create CoreContext.java

---
+++ src/test/java/Core/CoreContext.java
@@ -0,0 +1,24 @@
+/**
+ * Dana.id.
+ * Copyright (c) 2018‐2022 All Rights Reserved.
+ */
+package Core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Riza Nansuri
+ * @version $Id: CoreContext.java, v 0.1 2022‐01‐04 09.52 Riza Nansuri Exp $$
+ */
+public class CoreContext {
+    private final Map<String, Object> attributes = new HashMap();
+
+    public void setAttribute(String name, Object value) {
+        this.attributes.put(name, value);
+    }
+
+    public Object getAttribute(String name) {
+        return attributes.get(name);
+    }
+}
